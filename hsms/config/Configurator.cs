30cb1a004ec846a74c965ab74ac50872912e3aee
plochish <plochish@gmail.com>
2020-03-02 18:15:57 +0900

    Add data item.

--- hsms/config/Configurator.cs
+++ hsms/config/Configurator.cs
@@ -12,255 +12,259 @@ using System.Xml;
 
 namespace Semi.Hsms.Messages
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public class Configurator
-    {
-        #region Class properties
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Configurator
+	{
+		#region Class properties
+		/// <summary>
+		/// 
+		/// </summary>
+		public IPAddress IpAddress { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Port { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public ushort T3 { get; private set; }
 
-        public IPAddress IpAddress { get; private set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int Port { get; private set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public ushort T3 { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public ushort T5 { get; private set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public ushort T5 { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public ushort T6 { get; private set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public ushort T6 { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public ushort T7 { get; private set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public ushort T7 { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public ushort T8 { get; private set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public ushort T8 { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public static ConfigurationBuilder Builder => new ConfigurationBuilder();
 
+		#endregion
 
-        public static ConfigurationBuilder Builder => new ConfigurationBuilder();
+		#region Class initialization
 
-        #endregion
+		/// <summary>
+		/// 
+		/// </summary>
+		private Configurator()
+		{
+		}
 
-        #region Class initialization
+		#endregion
 
-        /// <summary>
-        /// 
-        /// </summary>
-        private Configurator()
-        {
-        }
+		#region Class internal structs
 
-        #endregion
+		/// <summary>
+		/// 
+		/// </summary>
+		public class ConfigurationBuilder
+		{
+			#region Class constants
 
-        #region Class internal structs
+			/// <summary>
+			/// 
+			/// </summary>
+			public const ushort DEFAULT_T3 = 45;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public class ConfigurationBuilder
-        {
-            #region Class constants
+			/// <summary>
+			/// 
+			/// </summary>
+			public const ushort DEFAULT_T5 = 10;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            public const ushort DEFAULT_T3 = 45;
+			/// <summary>
+			/// 
+			/// </summary>
+			public const ushort DEFAULT_T6 = 5;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            public const ushort DEFAULT_T5 = 10;
+			/// <summary>
+			/// 
+			/// </summary>
+			public const ushort DEFAULT_T7 = 10;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            public const ushort DEFAULT_T6 = 5;
+			/// <summary>
+			/// 
+			/// </summary>
+			public const ushort DEFAULT_T8 = 5;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            public const ushort DEFAULT_T7 = 10;
+			#endregion
 
-            /// <summary>
-            /// 
-            /// </summary>
-            public const ushort DEFAULT_T8 = 5;
+			#region Class members
 
-            #endregion
+			/// <summary>
+			/// 
+			/// </summary>
+			private IPAddress _ipAddress;
 
-            #region Class members
+			/// <summary>
+			/// 
+			/// </summary>
+			private int _port;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            private IPAddress _ipAddress;
+			/// <summary>
+			/// 
+			/// </summary>
+			private ushort _t3 = DEFAULT_T3;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            private int _port;
+			/// <summary>
+			/// 
+			/// </summary>
+			private ushort _t5 = DEFAULT_T5;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            private ushort _t3 = DEFAULT_T3;
+			/// <summary>
+			/// 
+			/// </summary>
+			private ushort _t6 = DEFAULT_T6;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            private ushort _t5 = DEFAULT_T5;
+			/// <summary>
+			/// 
+			/// </summary>
+			private ushort _t7 = DEFAULT_T7;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            private ushort _t6 = DEFAULT_T6;
+			/// <summary>
+			/// 
+			/// </summary>
+			private ushort _t8 = DEFAULT_T8;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            private ushort _t7 = DEFAULT_T7;
+			#endregion
 
-            /// <summary>
-            /// 
-            /// </summary>
-            private ushort _t8 = DEFAULT_T8;
+			#region Class public methods
 
-            #endregion
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="t3"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder T3( ushort t3 )
+			{
+				_t3 = ( t3 < 0 || t3 >= 120 ) ? DEFAULT_T3 : t3;
 
-            #region Class public methods
+				return this;
+			}
 
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="t3"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder T3(ushort t3)
-            {
-                _t3 = (t3 < 0 || t3 >= 120) ? DEFAULT_T3 : t3;
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="ipAddress"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder IP( IPAddress ipAddress )
+			{
+				_ipAddress = ipAddress;
 
-                return this;
-            }
+				return this;
+			}
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="port"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder Port( int port )
+			{
+				_port = port;
+				return this;
+			}
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="t5"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder T5( ushort t5 )
+			{
+				_t5 = ( t5 < 0 || t5 >= 240 ) ? DEFAULT_T5 : t5;
 
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="ipAddress"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder IP(IPAddress ipAddress)
-            {
-                _ipAddress = ipAddress;
+				return this;
+			}
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="t6"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder T6( ushort t6 )
+			{
+				_t6 = ( t6 < 0 || t6 >= 240 ) ? DEFAULT_T6 : t6;
 
-                return this;
-            }
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="port"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder Port(int port)
-            {
-                _port = port;
-                return this;
-            }
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="t5"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder T5(ushort t5)
-            {
-                _t5 = (t5 < 0 || t5 >= 240) ? DEFAULT_T5 : t5;
+				return this;
+			}
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="t7"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder T7( ushort t7 )
+			{
+				_t7 = ( t7 < 0 || t7 >= 240 ) ? DEFAULT_T7 : t7;
 
-                return this;
-            }
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="t6"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder T6(ushort t6)
-            {
-                _t6 = (t6 < 0 || t6 >= 240) ? DEFAULT_T6 : t6;
+				return this;
+			}
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="t8"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder T8( ushort t8 )
+			{
+				_t7 = ( t8 < 0 || t8 >= 120 ) ? DEFAULT_T8 : t8;
 
-                return this;
-            }
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="t7"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder T7(ushort t7)
-            {
-                _t7 = (t7 < 0 || t7 >= 240) ? DEFAULT_T7 : t7;
+				return this;
+			}
 
-                return this;
-            }
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="t8"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder T8(ushort t8)
-            {
-                _t7 = (t8 < 0 || t8 >= 120) ? DEFAULT_T8 : t8;
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="c"></param>
+			/// <returns></returns>
+			public ConfigurationBuilder Copy( Configurator c )
+			{
+				_ipAddress = c.IpAddress;
+				_port = c.Port;
+				_t3 = c.T3;
+				_t5 = c.T5;
+				_t6 = c.T6;
+				_t7 = c.T7;
+				_t8 = c.T8;
 
-                return this;
-            }
+				return this;
+			}
 
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="c"></param>
-            /// <returns></returns>
-            public ConfigurationBuilder Copy(Configurator c)
-            {
-                _ipAddress = c.IpAddress;
-                _port = c.Port;
-                _t3 = c.T3;
-                _t5 = c.T5;
-                _t6 = c.T6;
-                _t7 = c.T7;
-                _t8 = c.T8;
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <returns></returns>
+			public Configurator Build()
+			{
+				var c = new Configurator
+				{
+					IpAddress = _ipAddress,
+					Port = _port,
+					T3 = _t3,
+					T5 = _t5,
+					T6 = _t6,
+					T7 = _t7,
+					T8 = _t8
+				};
 
-                return this;
-            }
+				return c;
+			}
 
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <returns></returns>
-            public Configurator Build()
-            {
-                var c = new Configurator
-                {
-                    IpAddress = _ipAddress,
-                    Port = _port,
-                    T3 = _t3,
-                    T5 = _t5,
-                    T6 = _t6,
-                    T7 = _t7,
-                    T8 = _t8
-                };
+			#endregion
+		}
 
-                return c;
-            }
-
-            #endregion
-        }
-
-        #endregion
-    }
+		#endregion
+	}
 }
