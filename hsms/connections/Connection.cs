5efb07af9b745d7bf3da10d76f5e0445adaf16b1
hpavolot <helen.pavolotska@flextrade.com>
2020-03-16 21:11:32 +0000

    Try to connect to Simulator

---
+++ hsms/connections/Connection.cs
@@ -0,0 +1,58 @@
+﻿#region Usings
+using Semi.Hsms.Messages;
+using System;
+using System.Net;
+using System.Net.Sockets;
+#endregion
+
+namespace Semi.Hsms.connections
+{
+	public class Connection
+	{
+
+
+		#region Class fields
+		/// <summary>
+		/// 
+		/// </summary>
+		private Socket socket;
+		/// <summary>
+		/// 
+		/// </summary>
+		private IPEndPoint endPoint;
+
+		#endregion
+
+		#region Class Initialization
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="configurator"></param>
+		public Connection(Configurator configurator)
+		{
+			endPoint = new IPEndPoint(configurator.IpAddress, configurator.Port);
+
+			socket = new Socket(configurator.IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+		}
+		#endregion
+
+		#region Class methods
+
+		public  void Start()
+		{
+			try
+			{
+				socket.Connect(endPoint);
+
+				Console.WriteLine("Socket connected to {0}",
+				   socket.RemoteEndPoint.ToString());
+			}
+			catch (Exception e)
+			{
+
+				Console.WriteLine(e);
+			}
+		}
+		#endregion
+	}
+}
--- testsuite/Program.cs
+++ testsuite/Program.cs
@@ -1,51 +1,38 @@
 ﻿#region Usings
-using Semi.Hsms.Messages;
-using System.Collections.Generic;
+using System.Net;
+using static Semi.Hsms.Messages.Configurator;
+using Semi.Hsms.connections;
+using Semi.Hsms.messages.control;
 #endregion
 
 namespace Semi.Hsms.TestSuite
 {
-  class Program
-  {
-    #region Class public methods
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="args"></param>
-    static void Main(string[] args)
-    {
-      var items = new List<DataItem>();
-      items.Add(new I2(32));
-      items.Add(new U2(2));
-      items.Add(new A("lena", 10));
-      items.Add(new A("denis", 3));
+	class Program
+	{
+		#region Class public methods
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			var ip = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];
 
-      var dataList = new ListItem(
-        new A( "mama", 5 ),
-        new ListItem(
-          new F8( 10.56 ),
-          new A( "test", 12 ) ) );
+			var config = new ConfigurationBuilder()
+				.IP(ip)
+				.Port(11000)
+				.Build();
 
-      var dataList2 = new ListItem(
-      new A( "mama", 5 ),
-      new A( "papa", 5 ) );
+			var connection = new Connection(config);
 
-      dataList.Equals( dataList2 );
+			connection.Start();
 
+			var selectReq = new SelectReq(1, 2);
+			connection.Send(selectReq);
+			
+			connection.Stop();
 
-
-      items.Add( dataList );
-
-      var message = DataMessage
-        .Builder
-        .Stream(1)
-        .Function(5)
-        .Context(522555)
-        .Items(items)
-        .Build();
-
-      var copy = message.Items as List<DataItem>;
-    }
-    #endregion
-  }
+		}
+		#endregion
+	}
 }
